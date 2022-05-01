428893f0f6b937d7be37ed530da6276b1388df44
Ryan Williams <ryanwilliams@Ryans-MBP.attlocal.net>
2022-05-01 15:56:46 -0700

    added features to HTML page

--- app.py
+++ app.py
@@ -14,24 +14,26 @@ def punctuation_removal(text):
     clean_str = ''.join(all_list)
     return clean_str
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def home():
     return render_template("index.html")
 
 @app.route("/", methods=['POST'])
 def fake_or_real():
-#     run formthrough machine learning model and render results
+    # retrieve user input 
     article = str(request.form["text"])
+    # prepare data for machine model with NLP preprossesing
     prepped_art = punctuation_removal(article)
     tok_article = word_tokenize(prepped_art)
     stop_words = stopwords.words('english')
     cleaned_article = " ".join([word for word in tok_article if word not in stop_words])
+    # machine learning model and render results
     prediction = ml_model.predict([cleaned_article])
     if prediction[0] == "true":
         output = "looks CREDIBLE!"
     else:
         output = "looks like FAKE NEWS!"
-    return render_template('index.html', prediction_text="Your article {}".format(output))
+    return render_template('index.html', prediction_text="Your article looks {}".format(output))
 
 if __name__ == "__main__":
     app.run(debug=True)
